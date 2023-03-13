e2e532697e95fc01a9659754af275c4ad428e707
Le_Vrai_WIWI <superwoup@gmail.com>
2023-03-13 17:08:38 +0100

    je suis pas venue ici pour souffrir ok

---
+++ index.php
@@ -0,0 +1,37 @@
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hot GPT</title>
+
+    <style>
+        body {
+            background-color: #343541;
+        }
+
+        h1 {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            font-size: 45px;
+        }
+
+        center {
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+    <center>
+        <br><br><br><br><br><br><br><br><br><br>
+        <h1>
+            HotGPT
+        </h1>
+
+        <H3>Exemples</H3>
+    </center>
+    <input type="text">
+</body>
+
+</html>
