bfa3cb4be0d562ded9b168d542d1fba2aab59d95
Le_Vrai_WIWI <superwoup@gmail.com>
2023-03-15 19:43:37 +0100

    akuna matat

--- index.php
+++ index.php
@@ -16,66 +16,8 @@ var_dump($discution);
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="style.css">
   <title>Hot GPT</title>
-
-  <style>
-    body {
-      background-color: #343541;
-      color: white;
-    }
-
-    h1 {
-      font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-      font-size: 45px;
-    }
-
-    center {
-      color: #FFACAC;
-    }
-
-    /* css pour la barre de saisis */
-    input[type="text"] {
-      width: 70%;
-      padding: 12px 20px;
-      margin: 8px 0;
-      box-sizing: border-box;
-      border: 2px solid #40414f;
-      margin-right: 2%;
-      background-color: #40414f;
-      color: white;
-      box-shadow: 0 0 10px black;
-    }
-
-    .input>form {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      position: fixed;
-      bottom: 20px;
-      left: 5%;
-      width: 90%;
-    }
-
-    button {
-      background-color: #FFACAC;
-      color: white;
-      padding: 14px 20px;
-      margin: 8px 0;
-      border: none;
-      cursor: pointer;
-      width: 10%;
-    }
-
-    button:hover {
-      opacity: 0.8;
-    }
-
-    center>.dialogue {
-      disdisplay: flex;
-      justify-content: center;
-      align-items: center;
-    }
-  </style>
 </head>
 
 <body>
